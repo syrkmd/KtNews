d4d4e53d4a96ff78fd0057449231c83911c4fb97
Yurii <yvla@MacBook-Pro-Yurii.local>
2026-08-17 23:13:58 +0400

    feat: add pending intent for notification

--- app/src/main/java/com/yvl/news/data/background/NotificationsHelper.kt
+++ app/src/main/java/com/yvl/news/data/background/NotificationsHelper.kt
@@ -2,11 +2,14 @@ package com.yvl.news.data.background
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.content.getSystemService
 import com.yvl.news.R
+import com.yvl.news.presentation.MainActivity
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -33,9 +36,22 @@ class NotificationsHelper @Inject constructor(
     }
 
     fun showNewArticlesNotification(topics: List<String>) {
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            PENDING_INTENT_REQUEST_CODE,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
         val notification = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_breaking_news)
             .setContentTitle(context.getString(R.string.new_articles_notification_title))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
             .setContentText(
                 context.getString(
                     R.string.update_subscriptions,
@@ -51,5 +67,6 @@ class NotificationsHelper @Inject constructor(
 
         private const val CHANNEL_ID = "new_articles"
         private const val NOTIFICATION_ID = 1
+        private const val PENDING_INTENT_REQUEST_CODE = 1
     }
 }
